e88276ca7b58d35b0f8e2c83dd7768fa5875f212
Wellenton Modesto <wellenton16@hotmail.com>
2022-04-16 15:39:39 -0300

    commit teste

--- modelo.ts
+++ modelo.ts
@@ -1,7 +1,13 @@
 interface Funcionario {
     nome: string;
-    salario: number;
+    salarioBruto: number;
+    valorTotalHorasExtra: number;
+    faixaDescontoINSS: number;
+    valorDescontoINSS: number;
     horaExtra: number;
+    faixaDescontoIR: number;
+    valorDescontoIR: number;
+    salarioLiquido: number;
 }
 
 class Funcionario {
@@ -12,18 +18,33 @@ class Funcionario {
     setNome(novoNome: string){
         this.dadosFuncionarios.nome = novoNome;
     }
+    setSalarioBruto(novoSalarioBruto: number){
+        this.dadosFuncionarios.salarioBruto = novoSalarioBruto;
+    }
+    setValorTotalHorasExtra(novoValorTotalHorasExtra: number){
+        this.dadosFuncionarios.valorTotalHorasExtra = novoValorTotalHorasExtra;
+    }
+    //
     getNome(): string {
         return this.dadosFuncionarios.nome;
     }
+    getSalarioBruto(): number {
+        return this.dadosFuncionarios.salarioBruto;
+    }
+    getValorTotalHorasExtra(): number{
+        return this.dadosFuncionarios.valorTotalHorasExtra;
+    }
 }
 
-function modelo(nome, salario, horasExtras) {
+function modelo(nome: string, salarioBruto: number, horasExtras: number) {
     console.log(nome);
-    console.log(salario);
+    console.log(salarioBruto);
     console.log(horasExtras);
     const funcionario = new Funcionario();
     funcionario.setNome(nome);
+    funcionario.setSalarioBruto(salarioBruto);
     console.log(funcionario.getNome());
+    console.log(funcionario.getSalarioBruto());
 }
 
 modelo(process.argv[2], process.argv[3], process.argv[4]);
